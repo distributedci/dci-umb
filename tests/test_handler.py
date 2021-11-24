f25d0b3c852a51d33b13502c63317dcf9562b623
François Charlier <francois.charlier@redhat.com>
2021-11-24 17:10:27 +0100

    Also sent properties/headers to dci-feeder
    There are some properties/headers which are interesting for the feeder
    in order to properly filter incoming messasges.
    
    Change-Id: I70b79b312eb70f312c35b06fd93b8e77fb0d6c65

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -6,15 +6,17 @@ from tests.factories import Event
 
 
 def test_is_interested_in_all_events():
-    event = Event({})
+    event = Event({}, {})
     handler = HTTPBouncerMessageHandler(destination="http://localhost:5000/events")
     assert handler.is_interested_in(event)
 
 
 @mock.patch("dci_umb.handler.requests.post")
 def test_HTTPBouncerMessageHandler_send_body_in_post_request(mocked_requests_post):
-    event = Event({"id": "e1"})
+    event = Event({"id": "e1"}, {"message-id": "ID:id1234"})
     destination = "http://localhost:5000/events"
     handler = HTTPBouncerMessageHandler(destination=destination)
     handler.handle_event(event)
-    mocked_requests_post.assert_called_once_with(destination, json={"id": "e1"})
+    mocked_requests_post.assert_called_once_with(
+        destination, json={"msg": {"id": "e1"}, "headers": {"message-id": "ID:id1234"}}
+    )
