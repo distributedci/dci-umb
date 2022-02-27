6462f7df898a88621c64ab09f7df0b9d83ba1a9c
Guillaume Vincent <gvincent@redhat.com>
2022-02-27 23:23:52 +0000

    Message properties is already a dict
    Change-Id: Icec85f54037af8167ec7feed5551ede1aaf56adf

--- tests/factories.py
+++ tests/factories.py
@@ -7,5 +7,5 @@ class Event(object):
     def __init__(self, body, properties):
         self.message = Message(
             body=json.dumps(body).encode("utf-8"),
-            properties=json.dumps(properties).encode("utf-8"),
+            properties=properties,
         )
